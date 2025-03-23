c5bd724ee69efd564cefa19fb5fb5b1b6c0e952a
NoHyeokPark <park92y@naver.com>
2025-03-24 02:16:07 +0900

    HW0323

---
+++ icecream/IcecreamMain.java
@@ -0,0 +1,12 @@
+package house.persnal.re.icecream;
+
+public class IcecreamMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("아이스크림 판매점에 오신것을 환영합니다");
+		Mart m = new Mart();
+		m.ICFreezer();
+	}
+
+}
