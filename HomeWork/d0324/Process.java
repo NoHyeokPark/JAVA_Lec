ffee206abe25bf88cfe99806724d358c1b8ee275
NoHyeokPark <park92y@naver.com>
2025-03-24 18:31:32 +0900

    Linux

---
+++ HomeWork/d0324/Process.java
@@ -0,0 +1,11 @@
+public class Process {
+	int pid;
+	int burstTime;
+	int waitingTime;
+	int turnaroundTime;
+
+	public Process(int pid, int burstTime){
+		this.pid = pid;
+		this.burstTime = burstTime;
+	}
+}
