619f15d229abe158214bdcce87d60d09d4771275
NoHyeokPark <park92y@naver.com>
2025-04-01 19:46:45 +0900

    beak

--- FreeStudy/FlyCentauri.java
+++ FreeStudy/FlyCentauri.java
@@ -2,7 +2,7 @@ package FreeStudy;
 
 import java.util.Scanner;
 
-public class Main {
+public class FlyCentauri {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
