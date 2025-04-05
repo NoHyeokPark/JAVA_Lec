76c7e0d7032a7aeea0d87f3cb385fc3615d1ccd9
NoHyeokPark <park92y@naver.com>
2025-04-05 22:45:27 +0900

    Baekjun

--- FreeStudy/B1049.java
+++ FreeStudy/B1049.java
@@ -2,8 +2,7 @@ package FreeStudy;
 
 import java.util.Scanner;
 
-public class Main {
-
+public class B1049 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -35,5 +34,4 @@ public class Main {
 		}
 
 	}
-
 }
