cb5e641039103ff387593b70e4082ae9c2f4ae0e
NoHyeokPark <park92y@naver.com>
2025-04-15 21:33:55 +0900

    joon

---
+++ baekjoon/B24723.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B24723 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println((int)Math.pow(2,a));
+
+	}
+}
