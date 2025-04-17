d3505bcec3385833effd308f45587c1ab12d6076
NoHyeokPark <park92y@naver.com>
2025-04-17 20:36:37 +0900

    joon

--- FreeStudy/B16968.java
+++ FreeStudy/B16968.java
@@ -1,10 +1,8 @@
-
 package FreeStudy;
 
 import java.util.Scanner;
 
-public class Main {
-
+public class B16968 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
