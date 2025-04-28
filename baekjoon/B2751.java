25218881e3ee3d61dee0f2a23987ae5cf2f2d1cc
NoHyeokPark <park92y@naver.com>
2025-04-28 21:31:45 +0900

    joon

--- baekjoon/B2751.java
+++ baekjoon/B2751.java
@@ -1,28 +1,22 @@
+package baekjoon;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
-public class Main {
-
+public class B2751 {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
-		int[] arrx = new int[n];
-		int[] arry = new int[n];
+		int[] arr = new int[n];
 		for(int i = 0 ; i < n; i++) {
-			arrx[i] = Integer.parseInt(br.readLine());
-			arry[i] = Integer.parseInt(br.readLine());
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		for(int i = 0 ; i < n-1 ; i++)  {
-			for(int j = 0; j < n-1; j++) {
-				
-			}
-		}
-		
+		Arrays.sort(arr);
 		for(int i = 0 ; i < n; i++) {
 			bw.write(String.valueOf(arr[i]));
             bw.newLine();
@@ -31,5 +25,4 @@ public class Main {
         br.close();
         bw.close();		
 	}
-
 }
